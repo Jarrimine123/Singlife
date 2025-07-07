3d81e1d351cb6d40b3e458bc483f2e0b7ac18389
LOW JIE MIN <musicaljiemin@gmail.com>
2025-07-07 13:04:47 +0800

    rfeq

---
+++ StaffHomePage.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="StaffHomePage.aspx.cs" Inherits="Singlife.StaffHomePage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headTitle" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ StaffLogin.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Singlife
+{
+    public partial class StaffLogin : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+    
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text;
+
+            // Hardcoded staff credentials
+            string staffUsername = "admin";
+            string staffPassword = "admin123"; // Change this to whatever you want
+
+            if (username == staffUsername && password == staffPassword)
+            {
+                // You can redirect to a staff dashboard or admin page
+                Response.Redirect("StaffHomePage.aspx");
+            }
+            else
+            {
+                lblError.Text = "Invalid staff credentials.";
+            }
+        }
+    }
+}
